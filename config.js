6203ad985df6cb619254c7bf05319683155ddddd
Sarkar~Tech <bandahealimaree@gmail.com>
2024-12-28 05:02:50 +0500

    Update config.js

--- config.js
+++ config.js
@@ -22,6 +22,7 @@ module.exports = {
     AUTO_REACT: process.env.AUTO_REACT || "true",
     HEART_REACT: process.env.HEART_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "true",
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923253617422",
     BOT_NAME: process.env.BOT_NAME || "➺BALOCH-MD࿐",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
