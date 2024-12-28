263644085f668f055d475b8c1d55a133bac3be09
Sarkar~Tech <bandahealimaree@gmail.com>
2024-12-28 05:10:29 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: process.env.SESSION_ID || "o0gSFBBJ#Qx8-tTCVBO681lJ7h4yOjpmcQj25IRByqfqsfQFehX8",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
-    READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
-    MODE: process.env.MODE || "public",
-    AUTO_VOICE: process.env.AUTO_VOICE || "true",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
+    MODE: process.env.MODE || "private",
+    AUTO_VOICE: process.env.AUTO_VOICE || "private",
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
     ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Sarkar-Bandaheali/BALOCH-MD_DATABASE/refs/heads/main/autovoice/file-KegkWLpfvrRHPJDpD5iTRB%20(1).webp",
@@ -18,10 +18,10 @@ module.exports = {
     ANTI_LINK: process.env.ANTI_LINK || "true",
     ANTI_BAD: process.env.ANTI_BAD || "true",
     PREFIX: process.env.PREFIX || ".",
-    FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
-    AUTO_REACT: process.env.AUTO_REACT || "true",
-    HEART_REACT: process.env.HEART_REACT || "true",
-    OWNER_REACT: process.env.OWNER_REACT || "true",
+    FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    HEART_REACT: process.env.HEART_REACT || "false",
+    OWNER_REACT: process.env.OWNER_REACT || "false",
     OWNER_NUMBER: process.env.OWNER_NUMBER || "923253617422",
     BOT_NAME: process.env.BOT_NAME || "➺BALOCH-MD࿐",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
